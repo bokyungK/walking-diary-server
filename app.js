0146c6fba1a89ce89722a954c8c176abba91ff47
freebob2b <freebob2b@gmail.com>
2022-10-06 23:28:54 +0900

    feat: add basic login feature

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs').promises;
 const mysql = require('mysql2');
 
+// database 연동
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,11 +18,29 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.post('/user', function (req, res) {
+
+
+// 회원가입 
+app.post('/join', function (req, res) {
     const { userId, userPw, userName, userPetName } = req.body;
     db.query(`INSERT INTO user(id, pw, name, dog_name) VALUES('${userId}', '${userPw}', '${userName}', '${userPetName}')`);
-    res.send(typeof userId);
+    res.send('Success!');
 });
+
+// 로그인
+app.post('/login', function (req, res) {
+    const { userId, userPw } = req.body;
+    userInfo = db.query(`SELECT pw FROM user WHERE id='${userId}'`, function (err, result, fields) {
+        if (result[0].pw === userPw) {
+            res.send('로그인 성공');
+        } else {
+            res.send('로그인 실패');
+        }
+    });
+})
+
+
+
 app.listen(3001, () => {
-    console.log('익스프레스 서버 실행');
+    console.log('Server is running!');
 });
