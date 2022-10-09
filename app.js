85fee847c85c3c25ae2ef4c0863c422c6b6d0331
freebob2b <freebob2b@gmail.com>
2022-10-10 02:28:33 +0900

    feat: Apply password encryption

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
-const fs = require('fs').promises;
 const mysql = require('mysql2');
 
 // database 연동
@@ -21,24 +20,26 @@ app.use(express.urlencoded({ extended: false }));
 
 
 // 회원가입 
-app.post('/join', function (req, res) {
+app.post('/join', async function (req, res) {
     const { userId, userPw, userName, userPetName } = req.body;
-    db.query(`INSERT INTO user(id, pw, name, dog_name) VALUES('${userId}', '${userPw}', '${userName}', '${userPetName}')`);
-    res.send('Success!');
+    const hash = await argon2.hash(userPw);
+    db.query(`INSERT INTO user(id, pw, name, dog_name) VALUES('${userId}', '${hash}', '${userName}', '${userPetName}')`,
+    function (err, result, fields) {
+        res.send('Success');
+    });
 });
 
 // 로그인
 app.post('/login', function (req, res) {
     const { userId, userPw } = req.body;
-    userInfo = db.query(`SELECT pw FROM user WHERE id='${userId}'`, function (err, result, fields) {
-        if (result[0].pw === userPw) {
-            res.send('로그인 성공');
+    userInfo = db.query(`SELECT pw FROM user WHERE id='${userId}'`, async function (err, result, fields) {
+        if (await argon2.verify(result[0].pw, userPw)) {
+            res.send('Success');
         } else {
-            res.send('로그인 실패');
+            res.send('Fail');
         }
     });
 })
-
 
 
 app.listen(3001, () => {
