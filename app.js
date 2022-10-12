416b7804183dc06be24f829a02ce951621a4f68f
freebob2b <freebob2b@gmail.com>
2022-10-12 19:43:27 +0900

    feat: Add dogs to My Page

--- app.js
+++ app.js
@@ -21,21 +21,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // join 
 app.post('/join', async function (req, res) {
-    const { userId, userPw, userName, userPetName } = req.body;
+    const { userId, userPw, userName } = req.body;
     const hash = await argon2.hash(userPw);
-    db.query(`INSERT INTO user(id, pw, name, dog_name) VALUES('${userId}', '${hash}', '${userName}', '${userPetName}')`,
+    db.query(`INSERT INTO user(id, pw, name) VALUES('${userId}', '${hash}', '${userName}')`,
     function (err, rows, fields) {
-        if (rows) {
-            res.send('Success');
-            return;
-        }
         if (err) {
             res.send('Fail');
+            return;
         }
-    });
+        res.send('Success');
+    })
 });
 
 // login
@@ -44,15 +41,18 @@ app.post('/login', function (req, res) {
     db.query(`SELECT pw FROM user WHERE id='${userId}'`, async function (err, rows, fields) {
         if (rows.length === 0) {
             res.send('Fail_id');
-            return
+            return;
         }
         if (await argon2.verify(rows[0].pw, userPw)) {
             const access_token = jwt.sign({ userId } , 'secure');
-            res.cookie('access_token', access_token);
+            res.cookie('access_token', access_token, {
+                httpOnly: true,
+                maxAge: 3600000,
+            });
             res.send('Success');
-        } else {
-            res.send('Fail_pw');
+            return;
         }
+        res.send('Fail_pw');
     });
 })
 
@@ -64,23 +64,90 @@ app.get('/info', function (req, res) {
         return;
     }
     const { userId } = jwt.verify(access_token, 'secure');
-    db.query(`SELECT name, id, dog_name FROM user WHERE id='${userId}'`, function(err, rows, fields) {
+    db.query(`SELECT name, id FROM user WHERE id='${userId}'`, function(err, rows, fields) {
         if (rows.length === 0) {
             res.send('This is not a valid token');
             return;
         }
-        res.send(rows);
+        const res1 = rows[0];
+        db.query(`SELECT dog_name_1, dog_name_2, dog_name_3 FROM dog WHERE id='${userId}'`, function(err, rows, fields) {
+            if (rows.length === 0) {
+                res.send(res1);
+                return;
+            }
+            const res2 = rows[0];
+            const res3 = Object.assign(res1, res2);
+            res.send(res3);
+        })
     })
 })
 
-// withdrawal
-app.get('/withdrawal', function (req, res) {
+app.post('/info', async function (req, res) {
     const { access_token } = req.cookies;
+    const { userPw, userNewPw, userDogName1, userDogName2, userDogName3 } = req.body;
+    const { userId } = jwt.verify(access_token, 'secure');
+    if (userNewPw !== '') {
+        if (!access_token) {
+            res.send('There is no access_token');
+            return;
+        }
+        db.query(`SELECT pw FROM user WHERE id='${userId}'`, async function(err, rows, fields) {
+            if (rows.length === 0) {
+                res.send('This is not a valid token');
+                return;
+            }
+            if (!await argon2.verify(rows[0].pw, userPw)) {
+                res.send('Password is not correct');
+                return;
+            }
+            const newHash = await argon2.hash(userNewPw);
+            db.query(`UPDATE user SET pw='${newHash}' WHERE id='${userId}'`, function(err, rows, fields) {
+                if (err) {
+                    res.send('This is not a valid token');
+                    return;
+                }
+                res.send('Success');
+            })
+        })
+    }
+    
+    const userDogNames = [userDogName1, userDogName2, userDogName3]
     if (!access_token) {
         res.send('There is no access_token');
         return;
     }
+    db.query(`SELECT pw FROM user WHERE id='${userId}'`, async function(err, rows, fields) {
+        if (rows.length === 0) {
+            res.send('This is not a valid token');
+            return;
+        }
+        if (!await argon2.verify(rows[0].pw, userPw)) {
+            res.send('Password is not correct');
+            return;
+        }
+    })
+    db.query(`SELECT * FROM dog WHERE id='${userId}'`, function(err, rows, fields) {
+        if (rows.length === 0) {
+            db.query(`INSERT INTO dog(id, dog_name_1, dog_name_2, dog_name_3) VALUES('${userId}', '${userDogName1}', '${userDogName2}', '${userDogName2}')`,
+            function(err, rows, fields) {
+                res.send('Success');
+            })
+            return;
+        }
+        db.query(`UPDATE dog SET dog_name_1='${userDogName1}', dog_name_2='${userDogName2}', dog_name_3='${userDogName3}' WHERE id='${userId}'`, function(err, rows, fields) {
+            res.send('Success');
+        })
+    })
+})
+
+// mypage-withdrawal
+app.get('/withdrawal', function (req, res) {
+    const { access_token } = req.cookies;
     const { userId } = jwt.verify(access_token, 'secure');
+    if (!access_token) {
+        res.send('There is no access_token');
+        return;
+    }
     db.query(`DELETE FROM user WHERE id='${userId}'`, function(err, rows, fields) {
         if (rows.length === 0) {
             res.send('This is not a valid token');
@@ -91,15 +158,15 @@ app.get('/withdrawal', function (req, res) {
     // dog, diary 테이블 정보도 삭제되도록 추가하기!
 })
 
-// logout
+// mypage-logout
 app.get('/logout', function (req, res) {
     const { access_token } = req.cookies;
+    const { userId } = jwt.verify(access_token, 'secure');
     if (!access_token) {
         res.send('There is no access_token');
         return;
     }
-    const { userId } = jwt.verify(access_token, 'secure');
-    db.query(`SELECT name, id, dog_name FROM user WHERE id='${userId}'`, function(err, rows, fields) {
+    db.query(`SELECT name, id FROM user WHERE id='${userId}'`, function(err, rows, fields) {
         if (rows.length === 0) {
             res.send('This is not a valid token');
             return;
